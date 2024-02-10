14ca644de73737d8dfa2ea8a3f944c1308b5b654
J Cesar Hernandez R <157755654+hernandezr-jcesar@users.noreply.github.com>
2024-02-09 22:29:24 -0600

    Add files via upload

---
+++ P2-IntroduccionAlSistemaOperativoLinuxYWindows2/punto10linux.c
@@ -0,0 +1,69 @@
+/*
+ 10. Una vez creados los archivos con sus 
+ contenidos por el programa del punto 8 y 
+ utilizando unicamente las llamadas al sistema 
+ revisadas para Linux que sean necesarias, 
+ desarrolar:
+ un programa en C que liste los archivos 
+ creados, mostrando su tamanio, fecha y hora de acceso.
+*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+
+//para poder usar el opendir, readdir
+#include <stdlib.h>
+#include <dirent.h>
+
+//simulacion del comando ls
+// comandos usados para este programa:
+// stat, opendir, readdir 
+int main()
+{
+	DIR *dir;
+	struct dirent *sd;
+
+	dir = opendir(".");
+
+	if(dir == NULL)
+	{
+		printf("Error! No se puede abrir el directorio\n");
+		exit(1);
+	}
+	char cadena[100];
+	char temp[100];
+	while ( (sd=readdir(dir)) != NULL )
+	{
+		//printf("-> %s\n", sd->d_name);
+		//fputs(cadena,sd->d_name);
+		sprintf(cadena,sd->d_name);
+		for(int conta=0;conta < strlen(cadena);conta++){			
+			if(cadena[conta] == '.'){
+				if(cadena[conta+1] == 't'){
+					if(cadena[conta+2] == 'x'){
+						if(cadena[conta+3] == 't'){
+
+							printf("-> %s\n", sd->d_name);
+							struct stat buf;
+							char mtime[100];
+
+							stat(sd->d_name, &buf);
+							printf("Tamanio: %ld bytes\n",buf.st_size);
+							strcpy(mtime, ctime(&buf.st_mtime));
+							printf("Ultimo Acceso:  %s\n", mtime);
+						}
+					}
+
+				} 
+			}
+
+		}
+	}
+	
+
+
+	return 0;
+}
